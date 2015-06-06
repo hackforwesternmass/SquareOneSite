e8ca6230b922f388753e82427b93c8333d4fe378
Jesse Evers <jesse27999@gmail.com>
2015-06-06 17:32:27 -0400

    this is working so i'm committing

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
+  get 'static_pages/index'
+
   resources :attendances
-  resources :children
   resources :providers
+  resources :static_pages
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
-  # root 'providers#index'
+   root 'static_pages#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
