6ad92217e505536b189fe09fe5f76ac846850178
hoffmanb <hoffmanb@elms.edu>
2015-08-02 21:18:16 +0000

    remove lname lowercase in provider db migrate

--- db/migrate/20150802204420_change_case_sensitive_column_names.rb
+++ db/migrate/20150802204420_change_case_sensitive_column_names.rb
@@ -1,7 +1,7 @@
 class ChangeCaseSensitiveColumnNames < ActiveRecord::Migration
   def change
    rename_column :providers, :lName, :lname
-   rename_column :providers, :fName, :fname
+  
 
   end
 end
