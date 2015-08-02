daaa1842eab5965efc4c114c0e37cac9a8d29b12
hoffmanb <hoffmanb@elms.edu>
2015-08-02 22:00:13 +0000

    Removed case migrations

--- db/migrate/20150802204420_change_case_sensitive_column_names.rb
+++
@@ -1,7 +0,0 @@
-class ChangeCaseSensitiveColumnNames < ActiveRecord::Migration
-  def change
-   rename_column :providers, :lName, :lname
-  
-
-  end
-end
--- db/migrate/20150802212730_fix_child_case.rb
+++
@@ -1,6 +0,0 @@
-class FixChildCase < ActiveRecord::Migration
-  def change
-         rename_column :children, :lName, :lname
-
-  end
-end
